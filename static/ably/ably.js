3fdc2d6458949f681754a82f2dd51c462d6c6473
fedi41-prog <crocofedi4@gmail.com>
2025-12-11 22:24:01 +0100

    pretty good version?

--- static/ably/ably.js
+++ static/ably/ably.js
@@ -1,21 +1,34 @@
+function getUrlParam(name) {
+    var url_string = window.location;
+    var url = new URL(url_string);
+    var c = url.searchParams.get(name);
+    return c;
+}
+
+
 async function setupAbly() {
     const realtimeClient = new Ably.Realtime({
-        key: 'HBc1Qw.EUDr4Q:4EjI-Zkt67PAu_OtlpxuvUvuPZ6Gt60Gs1-6SrU1-ss',
-        clientId: 'anonymos-user-' + Math.floor(Math.random() * 100000)
+        key: 'exK4Cg.m4VhmA:GDez8Ium7MNITXQ0MonMuwiVdC5TluhFbsDkem40itU',
+        clientId: 'User' + Math.floor(Math.random() * 100000)
     });
     await realtimeClient.connection.once('connected');
 
-    const channel = realtimeClient.channels.get('my-first-channel');
-    addMessage('LOG', 'Made my first connection!');
+    const channel = realtimeClient.channels.get(getUrlParam('channel') || 'global-chat');
+    addMessage('LOG', 'CONNECTED as ' + realtimeClient.auth.clientId);
 
     await channel.subscribe((message) => {
-        addMessage(message.clientId, message.data);
+        isMyMessage = message.clientId === realtimeClient.auth.clientId;
+        messageType = isMyMessage ? "my-message" : "normal";
+        addMessage(message.clientId, message.data, messageType);
     });
 
     button = document.getElementById("send-button");
     button.addEventListener("click", () => {
         const inputField = document.getElementById("message-input");
         const messageText = inputField.value;
+        if (messageText.trim() === "") {
+            return; // Leere Nachrichten nicht senden
+        }
         channel.publish('message', messageText);
         inputField.value = "";
     })
@@ -28,13 +41,14 @@ setupAbly();
 
 let messageCounter = 1;
 
-function addMessage(heading, text) {
+function addMessage(heading, text, messageType="normal") {
     const area = document.getElementById("messageArea");
 
     // Neues div erzeugen
     const msgDiv = document.createElement("div");
     msgDiv.className = "message";
     msgDiv.id = "message" + messageCounter;
+    msgDiv.dataset.type = messageType;
 
     const msgText = document.createElement("span");
     msgText.className = "message-text";
@@ -51,7 +65,3 @@ function addMessage(heading, text) {
     area.appendChild(msgDiv);
     messageCounter++;  
 }
-
-// Beispiel
-addMessage("LOG", "Hallo Welt!");
-addMessage("LOG", "Zweite Nachricht!");
